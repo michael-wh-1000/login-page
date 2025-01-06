6fc8a958b541eec9717cc273c59e3e67e244623d
Michael <arinaitwemichael9@gmail.com>
2025-01-06 15:06:26 +0300

    Some basic form changes

--- src/Components/CreateAccount.jsx
+++ src/Components/CreateAccount.jsx
@@ -1,26 +1,72 @@
-import logo from '../assets/images/google-icon.svg'
-import { Link } from 'react-router-dom';
+import logo from "../assets/images/google-icon.svg";
+import { Link } from "react-router-dom";
+import { useState } from "react";
 
 export const CreateAccount = () => {
-    return (
-        <div className="create-account">
-           <div className="content">
-           <div className="heading">Create Account</div>
-           <div className="subheading">Already have an account? <Link to="/login" className='login-link'>Sign in</Link></div>
-            <form className="account-form" action="">
-                <label>Full name</label>
-                <input type="text" id="full-name" className= "text-input" placeholder="Enter name"/>
-                <label>Email</label>
-                <input type="email" id="email-adress" className= "text-input" placeholder="Email"/>
-                <label>Password</label>
-                <input type="password" id="password" className= "text-input" placeholder="Password"/>
-                <button type="submit" className="submit-button">Create Account</button>
-                <button className='google-login'>
-                    <img src={logo} />
-                    <p>Sign up with google</p>
-                </button>
-            </form>
-           </div>
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData((prevFormData) => ({ ...prevFormData, [name]: value }));
+  };
+
+  return (
+    <div className="create-account">
+      <div className="content">
+        <div className="heading">
+          <p className="heading-big-text">Create Account</p>
+          <p className="subheading">
+            Already have an account?&nbsp;
+            <Link to="/login" className="login-link">
+              Sign in
+            </Link>
+          </p>
         </div>
-    );
-}
+        <form className="account-form" action="">
+          <label htmlFor="name">Full name</label>
+          <input
+            name="name"
+            type="text"
+            id="name"
+            className="text-input"
+            onChange={handleChange}
+            placeholder="Enter name"
+          />
+          <label htmlFor="email">Email</label>
+          <input
+            name="email"
+            type="email"
+            id="email"
+            onChange={handleChange}
+            className="text-input"
+            placeholder="Email"
+          />
+          <label htmlFor="password">Password</label>
+          <input
+            name="password"
+            type="password"
+            id="password"
+            className="text-input"
+            onChange={handleChange}
+            placeholder="Password"
+          />
+          <button
+            type="button"
+            className="submit-button"
+            onClick={() => console.log(formData)}
+          >
+            Create Account
+          </button>
+          <button className="google-login">
+            <img src={logo} />
+            <p>Sign up with google</p>
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,24 +1,56 @@
-import logo from '../assets/images/google-icon.svg'
-import { Link } from 'react-router-dom';
+import logo from "../assets/images/google-icon.svg";
+import { Link } from "react-router-dom";
+import { useState } from "react";
 
 export const LoginForm = () => {
-    return (
-        <div className="create-account">
-        <div className="content">
-        <div className="heading">Sign in</div>
-        <div className="subheading">Do not have account? <Link to="/" className='login-link'>Create account</Link></div>
-         <form className="account-form" action="">
-             <label>Email</label>
-             <input type="email" id="email-adress" className= "text-input" placeholder="Email"/>
-             <label>Password</label>
-             <input type="password" id="password" className= "text-input" placeholder="Password"/>
-             <button type="submit" className="submit-button">Sign in</button>
-             <button className='google-login'>
-                 <img src={logo} />
-                 <p>Sign in with google</p>
-             </button>
-         </form>
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData((prevFormData) => ({ ...prevFormData, [name]: value }));
+  };
+
+  return (
+    <div className="create-account">
+      <div className="content">
+        <div className="heading">
+          <p className="heading-big-text">Sign in</p>
+          <p className="subheading">
+            Do not have an account?&nbsp;
+            <Link to="/" className="login-link">
+              Create Account
+            </Link>
+          </p>
         </div>
-     </div>
-    );
-}
+        <form className="account-form" action="">
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            onChange={handleChange}
+            className="text-input"
+            placeholder="Email"
+          />
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            onChange={handleChange}
+            className="text-input"
+            placeholder="Password"
+          />
+          <button type="submit" className="submit-button">
+            Sign in
+          </button>
+          <button className="google-login">
+            <img src={logo} />
+            <p>Sign in with google</p>
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
